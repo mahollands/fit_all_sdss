ef9ebe83152ab4a45d9f5c987802061ba6fda3aa
mahollands <M.Hollands.1@warwick.ac.uk>
2020-05-14 12:37:56 +0100

    Fsdgfsdgfhsdgfhdgfh

--- fit_sdss_templates.c
+++ fit_sdss_templates.c
@@ -29,18 +29,8 @@
 #include <omp.h>
 
 //#define N_SDSS 5789198
-<<<<<<< HEAD
-<<<<<<< HEAD
 //#define N_SDSS 578920
 #define N_SDSS 1000
-=======
-//#define N_SDSS 57892
-#define N_SDSS 10000
->>>>>>> parent of c92f907... interp and chi2 functions pass by reference for 10% perfomance boost
-=======
-//#define N_SDSS 578920
-#define N_SDSS 1000
->>>>>>> tmp
 #define N_PX_MAX 6000
 #define N_TEMPLATES 3078
 
@@ -161,17 +151,11 @@ void load_templates(template *TT)
       printf("Error opening %s\n", tname_full);
       exit(EXIT_FAILURE);
     }
-<<<<<<< HEAD
-    for(j=0; j<T.N; ++j)
-    {
-      fscanf(input, "%lf %lf%*s\n", &T.x[j], &T.y[j]);
-=======
 
     /*Read first two columns from template file*/
     for(j=0; j<T.N; ++j)
     {
       fscanf(input, "%lf %lf%*[^\n]\n", &T.x[j], &T.y[j]);
->>>>>>> tmp
     }
     fclose(input);
   }
